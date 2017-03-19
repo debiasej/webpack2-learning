a8b7e9dee6cbb7a1ed6ec87d556f73bad0a2f011
Mario de Biase <debiasej@gmail.com>
2017-03-19 22:40:46 +0100

    Extract styles from bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path'); // We can use whatever nodejs package
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -13,11 +14,17 @@ const config = {
         test: '/\.js$/'
       },
       {
-        use: ['style-loader', 'css-loader'], // it's apply from right to left
+        //use: ['style-loader', 'css-loader'], // it's apply from right to left
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }), // legacy way
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
