e0e8ab8266cec76e64746a1f6ff7b1ae5b7076a5
Mario de Biase <debiasej@gmail.com>
2017-03-20 19:08:32 +0100

    Example of codesppliting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 //const sum = require('./sum'); // CommonJS
 import sum from './sum'; // ES2015 (ES6)
-import './image_viewer';
+//import './image_viewer';
 
 const total = sum(10, 5);
 console.log(total);
+
+// Codesplitting
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+  System.import('./image_viewer').then(module => {
+    module.default();
+  });
+};
+
+document.body.appendChild(button);
